5c25d585c9c3faba4e2c19a798703ab31aec041a
Your Name <you@example.com>
2025-10-21 20:46:52 +0500

    fixing bugs

--- jay_e.sh
+++
@@ -1,4 +0,0 @@
-for f in $(git ls-files | grep ":"); do
-  new=$(echo "$f" | tr ':' '_')
-  echo "$f -> $new"
-done
--- jay_s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in $(git ls-files | grep ":"); do
-  new=$(echo "$f" | tr ':' '_')
-  echo "Renaming $f -> $new"
-  git mv "$f" "$new"
-done
-
-git commit -m "rename all ':' to '_'"
-git push
